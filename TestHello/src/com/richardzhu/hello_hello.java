a50211c41b4d574d64ea18eb84632a7be0fffb7b
richard-zy-zhu <rzhu@bellsouth.net>
2018-04-15 17:12:18 -0400

    test 4

--- TestHello/src/com/richardzhu/hello_hello.java
+++ TestHello/src/com/richardzhu/hello_hello.java
@@ -2,7 +2,7 @@ package com.richardzhu;
 
 public class hello_hello {
 	public static void main (String args[]) {
-		System.out.println("hello world, are your there?");
+		System.out.println("hello world, are your there right now?");
 	}
 	
 }
