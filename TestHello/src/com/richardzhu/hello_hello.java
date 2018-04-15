bb7f64b287ce63dbd43a42eb8efdf4a30aae5f83
RichardZhu <RichardZhu@RichardZhu-HP>
2018-04-15 18:54:30 -0400

    test new branch

--- TestHello/src/com/richardzhu/hello_hello.java
+++ TestHello/src/com/richardzhu/hello_hello.java
@@ -2,7 +2,7 @@ package com.richardzhu;
 
 public class hello_hello {
 	public static void main (String args[]) {
-		System.out.println("hello world, are your there right now?");
+		System.out.println("hello world, are your there right now? I am in dev branch now.");
 	}
 	
 }
