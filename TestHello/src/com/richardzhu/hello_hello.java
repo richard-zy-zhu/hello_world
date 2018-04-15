cc01bf5338e64790f9d01b2448d95c7a3e661f32
RichardZhu <RichardZhu@RichardZhu-HP>
2018-04-15 17:02:33 -0400

    test commit 2

--- TestHello/src/com/richardzhu/hello_hello.java
+++ TestHello/src/com/richardzhu/hello_hello.java
@@ -2,7 +2,7 @@ package com.richardzhu;
 
 public class hello_hello {
 	public static void main (String args[]) {
-		System.out.println("hello world");
+		System.out.println("hello world, are your there?");
 	}
 	
 }
