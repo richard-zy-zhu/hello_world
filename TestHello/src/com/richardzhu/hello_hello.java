b28b10a5e6205ad8cdfb488d61b21ce613dfa423
richard-zy-zhu <rzhu@bellsouth.net>
2024-09-13 00:41:17 -0400

    change in master

--- TestHello/src/com/richardzhu/hello_hello.java
+++ TestHello/src/com/richardzhu/hello_hello.java
@@ -2,6 +2,7 @@ package com.richardzhu;
 
 public class hello_hello {
 	public static void main (String args[]) {
+		System.out.println("change in master");
 		System.out.println("hello world, are your there right now?");
 	}
 	
